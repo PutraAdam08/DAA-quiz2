6057bb2cb7cb756f95b27704e34004c7c74db35b
MoChikayo <127732169+MoChikayo@users.noreply.github.com>
2024-05-24 13:42:34 +0700

    level

--- SudokuOps.hpp
+++ SudokuOps.hpp
@@ -33,5 +33,43 @@ class Sudops : private sudoku{
             return !UsedInRow(s, row, num) && !UsedInCol(s, col, num) && !UsedInBox(s, row - row % 3, col - col % 3, num) && s[row][col] == 0;
         }
 
+        void level(int difficulty) {
+            // Clear the current sudoku board
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    assign(i, j, 0);
+                    setFixed(i, j, false);
+                }
+            }
+
+            int prefilledCells;
+            switch (difficulty) {
+                case 1: // Easy
+                    prefilledCells = 36;
+                    break;
+                case 2: // Medium
+                    prefilledCells = 27;
+                    break;
+                case 3: // Hard
+                    prefilledCells = 18;
+                    break;
+                default:
+                    prefilledCells = 36; // Default to Easy
+            }
+
+            srand(time(0));
+            while (prefilledCells > 0) {
+                int row = rand() % 9;
+                int col = rand() % 9;
+                int num = (rand() % 9) + 1;
+
+                if (isSafe(sudoArr, row, col, num)) {
+                    assign(row, col, num);
+                    setFixed(row, col, true);
+                    prefilledCells--;
+                }
+            }
+        }
+
 
 };
